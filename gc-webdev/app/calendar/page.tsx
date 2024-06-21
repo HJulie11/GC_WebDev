f0cdda7617012fe9d25811b2fcb977febbf9449f
HJulie11 <Hseojin0711@gmail.com>
2024-06-21 20:18:10 +0900

    layout fixed

--- gc-webdev/app/calendar/page.tsx
+++ gc-webdev/app/calendar/page.tsx
@@ -1,16 +1,20 @@
 "use client";
 
 import Calendar from '../component/Calendar';
+import MainLayout from '../component/MainLayout';
 
 export default function Home() {
   return (
-    <div className="flex center items-center justify-center mt-10 mb-10">
+    <MainLayout>
+      <div className="flex center items-center justify-center mt-10 mb-10">
         <div className="container">
-            <main className="main">
-                <h1 className="flex center items-center justify-center mt-5 mb-5 font-bold text-[30px] text-purple-heavy">캘린더</h1>
-                <Calendar />
-            </main>
+          <main className="main">
+            <h1 className="flex center items-center justify-center mt-5 mb-5 font-bold text-[30px] text-purple-heavy">캘린더</h1>
+            <Calendar />
+          </main>
         </div>
-    </div>
+      </div>
+    </MainLayout>
+    
   );
 }
