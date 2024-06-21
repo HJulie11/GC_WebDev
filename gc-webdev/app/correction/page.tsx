fa03b441c89f37784aa3bcc2a355673d226e6926
Amishi Gangwar <92440009+angwar-g@users.noreply.github.com>
2024-06-21 20:53:40 +0530

    Update correction

--- gc-webdev/app/correction/page.tsx
+++ gc-webdev/app/correction/page.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import Dictation_textbox from '../component/Dictation_textbox';
-import Link from 'next/link';
-import Button from '../component/Button';
+import React from 'react'
+import Dictation_textbox from '../component/Dictation_textbox'
+import Link from 'next/link'
 
-const CorrectionPage = () => {
+const DictationPage = () => {
   return (
-    <div className='flex flex-row mt-6 p-10 min-h-screen space-x-5'>
-      <div className='flex-1 flex flex-col'>
-        <Dictation_textbox />
-      </div>
-      <div className='flex-1 flex flex-col'>
-        <Dictation_textbox />
-        <div className='mt-2'>
-          <Button type='button' title={<Link href="/correction">제출하기</Link>} variant='bg-gray-30' full={false} />
+    <>
+        <div className='flex flex-row p-40 h-[1000px]'>
+            <Dictation_textbox />
+            <div className='flex flex-col w-[40%] h-full ml-5'>
+            <Dictation_textbox />
+            <Link href="/reading">
+              <button style={{
+                display: 'flex',
+                width: '170px',
+                height: '50px',
+                padding: '12px 39px 13px 40px',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexShrink: 0,
+                borderRadius: '10px',
+                background: '#8E8E8E'
+              }} className='mt-40 ml-20'>제출하기</button>
+            </Link>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    </>
+  )
 }
 
-CorrectionPage.getLayout = function getLayout(page: React.ReactNode) {
-  return page;
+DictationPage.getLayout = function getLayout(page: React.ReactNode) {
+    return page;
 };
 
-export default CorrectionPage;
+export default DictationPage
