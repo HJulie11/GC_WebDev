e52ce887636bb904f01690f74e1dbd4d91499662
HJulie11 <Hseojin0711@gmail.com>
2024-08-01 01:32:19 +0900

    back to normal

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import { loginUser, registerUser, getUserProfile } from '../controllers/usercontroller.js';
+import { loginUser, registerUser } from '../controllers/usercontroller.js';
  
 const userRouter = express.Router();
  
 userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
-userRouter.get('/profile', getUserProfile);
  
 export default userRouter;
