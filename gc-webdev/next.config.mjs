d947ee70ab355a62ff68cd38ea757390c67adeb7
HJulie11 <Hseojin0711@gmail.com>
2024-08-24 12:49:21 +0900

    idk if it will work

--- gc-webdev/next.config.mjs
+++ gc-webdev/next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",  // <=== enables static exports
-    basePath: '/GC_WebDev/gc-webdev',
+    // basePath: '/GC_WebDev/gc-webdev',
+    basePath: process.env.NODE_ENV === 'production' ? '/gc-webdev' : '',
     reactStrictMode: true,
 };
 
