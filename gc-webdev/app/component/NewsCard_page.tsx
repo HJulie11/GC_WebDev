0022006d84f23594b35d5d79a4fcb33490e468fe
Charvi Agarwal <charviagarwal2002@gmail.com>
2024-08-26 11:38:53 +0100

    testing

--- gc-webdev/app/component/NewsCard_page.tsx
+++ gc-webdev/app/component/NewsCard_page.tsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import Card from './NewsCard'
+import Link from 'next/link'
+
 // TODO: make props interface to get different progress bars
 
+interface VideoItem {
+  id: {
+    videoId: string;
+  };
+  snippet: {
+    title: string;
+    thumbnails: {
+      medium: {
+        url: string;
+      };
+    };
+  };
+}
+
 const NewsCard_page = () => {
+  const [cnnVideos, setCnnVideos] = useState<VideoItem[]>([]);
+  const [bbcVideos, setBbcVideos] = useState<VideoItem[]>([]);
+
+  useEffect(() => {
+    fetch(
+      "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCupvZG-5ko_eiXAupbDfxWw&maxResults=10&order=date&key=AIzaSyDpref1bV9m7s7igwJqZG1JhC_gsXX60L0"
+    )
+    .then((response) => response.json())
+    .then((data) => {
+      setCnnVideos(data.items);
+    })
+  }, []);
+
+  return (
+    <>
+      {cnnVideos.map((video) => (
+        <Card
+          key={video.id.videoId}
+          title={video.snippet.title}
+          url={`https://www.youtube.com/watch?v=${video.id.videoId}`}
+          thumbnail={video.snippet.thumbnails.medium.url}
+          />
+      ))}
+    </>
+  )
+  
   return (
     <>
     <a href='dummyurl' className='flex flex-col w-[320px] shadow-md mr-5 mb-5 rounded-lg'>
