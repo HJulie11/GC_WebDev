4a681ca8328c24921054b5ba7571a5e4fdce06bf
Charvi Agarwal <charviagarwal2002@gmail.com>
2024-08-26 14:05:12 +0100

    working page but need to separate videos

--- gc-webdev/app/component/NewsCard_page.tsx
+++ gc-webdev/app/component/NewsCard_page.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import Link from 'next/link'
 import { title } from 'process';
+import { vi } from 'date-fns/locale';
 
 // TODO: make props interface to get different progress bars
 
@@ -29,7 +30,15 @@ const NewsCard_page = () => {
     .then((response) => response.json())
     .then((data) => {
       setCnnVideos(data.items);
-    })
+    });
+
+    fetch(
+       "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC16niRr50-MSBwiO3YDb3RA&maxResults=10&order=date&key=AIzaSyDpref1bV9m7s7igwJqZG1JhC_gsXX60L0"
+    )
+    .then((response) => response.json())
+    .then((data) => {
+      setBbcVideos(data.items)
+    });
   }, []);
 
   return (
@@ -60,8 +69,35 @@ const NewsCard_page = () => {
             </a>
           </Link>
       ))}
+
+      {bbcVideos.map((video) => (
+        <Link legacyBehavior
+          key={video.id.videoId}
+          href={{
+            pathname: `/dictation`,
+            query: {
+              title: video.snippet.title,
+              url: `https://www.youtube.com/watch?v=${video.id.videoId}`,
+              thumbnail: video.snippet.thumbnails.medium.url,
+              key: video.id.videoId
+            },
+          }}
+          passHref
+        >
+          <a className='flex flex-col w-[320px] shadow-md mr-5 mb-5 rounded-lg'>
+            <div className='w-[320px] h-[180px] rounded-top-right-lg round-top-left-lg'>
+              <img src={video.snippet.thumbnails.medium.url} alt={video.snippet.title} width={320} height={180} />
+            </div>
+            <div className='w-[300px] flex flex-col'>
+              <div className='font-semi-bold text-[20px] ml-5 mt-5'>{video.snippet.title}</div>
+              <div className='w-[250px] h-[10px] rounded-full bg-gray-30 mt-5 ml-5'></div>
+              <div className='text-[15px] ml-5 mt-5 mb-5'>Not started</div>
+            </div>
+          </a>
+        </Link>
+      ))}
     </>
-  )
+  );
 }
 
 export default NewsCard_page
