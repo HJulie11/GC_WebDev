93fc7daf2493974fbd3afb96f86e624e8494560f
Charvi Agarwal <charviagarwal2002@gmail.com>
2024-08-29 12:24:41 +0100

    dictation-level-one folder

---
+++ gc-webdev/app/dictation_level_one/layout.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+export default function LayoutDictation({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <main className="relative overflow-hidden">
+        {children}
+    </main>
+  )
+}
